a203df28e697e67e217968ccb40f49bcc1b3945e
alexalotl <32069762+alexalotl@users.noreply.github.com>
2025-12-02 00:11:41 +0000

    Chocolate cake....

--- src/main/java/chocolotl/Chocolotl.java
+++ src/main/java/chocolotl/Chocolotl.java
@@ -1,8 +1,11 @@
 package chocolotl;
 
+import chocolotl.blocks.ChocolateCakeBlock;
 import chocolotl.items.CocoaPowderItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -11,10 +14,14 @@ import net.minecraft.util.Identifier;
 public class Chocolotl implements ModInitializer {
 
     public static final Item COCOA_POWDER = new CocoaPowderItem(new FabricItemSettings());
+    public static final Block CHOCOLATE_CAKE = new ChocolateCakeBlock(Block.Settings.create().strength(0.5f));
 
     @Override
     public void onInitialize() {
 
         Registry.register(Registries.ITEM, Identifier.of("chocolotl", "cocoa_powder"), COCOA_POWDER);
+
+        Registry.register(Registries.ITEM, Identifier.of("chocolotl", "chocolate_cake"), new BlockItem(CHOCOLATE_CAKE, new Item.Settings()));
+        Registry.register(Registries.BLOCK, Identifier.of("chocolotl", "chocolate_cake"), CHOCOLATE_CAKE);
     }
 }
---
+++ src/main/java/chocolotl/blocks/ChocolateCakeBlock.java
@@ -0,0 +1,20 @@
+package chocolotl.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
+public class ChocolateCakeBlock extends Block {
+    public ChocolateCakeBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        return Block.createCuboidShape(1.0f, 0f, 1.0f, 15.0f, 8.0f, 15.0f);
+    }
+}
